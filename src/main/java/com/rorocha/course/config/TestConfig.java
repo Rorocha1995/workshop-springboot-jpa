357a9c31176a56349f910d50ad3585a6a1ee1173
Rorocha1995 <166776446+Rorocha1995@users.noreply.github.com>
2025-03-30 22:10:22 -0300

    OrderStatus enum

--- src/main/java/com/rorocha/course/config/TestConfig.java
+++ src/main/java/com/rorocha/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.rorocha.course.config;
 
 import com.rorocha.course.entities.Order;
 import com.rorocha.course.entities.User;
+import com.rorocha.course.entities.enums.OrderStatus;
 import com.rorocha.course.repositories.OrderRepository;
 import com.rorocha.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null,"Maria brown","maria@gmail.com","199999999","123456");
         User u2 = new User(null,"Alex Green","alex@gmail.com","2133333333","123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 
 
         userRepository.saveAll(Arrays.asList(u1,u2));
